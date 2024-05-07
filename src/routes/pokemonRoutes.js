852f190e168106450502164d75541319eb471080
Sergio Ferrari Bryce <106128316+Kyriokes@users.noreply.github.com>
2024-05-07 18:36:18 -0300

    Update pokemonRoutes.js

--- src/routes/pokemonRoutes.js
+++ src/routes/pokemonRoutes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const router = Router();
-const { getPokemonHandler, getPokemonIdHandler, createPokemonHandler } = require('./handlers/pokemonHandler')
+const { getPokemonHandler, getPokemonIdHandler, createPokemonHandler, DeletePokemonIdHandler } = require('./handlers/pokemonHandler')
 
 router.get("/", getPokemonHandler); // GET | /pokemons && GET | /pokemons/name?="..."
 router.get("/:id", getPokemonIdHandler); // GET | /pokemons/:idPokemon
 router.post("/", createPokemonHandler); //  POST | /pokemons
+router.delete("/:id", DeletePokemonIdHandler); // DELETE | /pokemons/:idPokemon
 
 module.exports = router;
