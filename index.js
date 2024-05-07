ece9cbde92cbd8447b9941daf6b2db20b747f9e9
Sergio Ferrari Bryce <106128316+Kyriokes@users.noreply.github.com>
2024-05-07 18:35:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-// Syncing all the models at once.
+const port = 8000
+
 conn.sync({ alter: true }).then(() => {
-  server.listen(8000, () => {
-    console.log('server is listening at 8000'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`server is listening at ${port}`); // eslint-disable-line no-console
   });
 });
-
-  //                       _oo0oo_
-  //                      o8888888o
-  //                      88" . "88
-  //                      (| -_- |)
-  //                      0\  =  /0
-  //                    ___/`---'\___
-  //                  .' \\|     |// '.
-  //                 / \\|||  :  |||// \
-  //                / _||||| -:- |||||- \
-  //               |   | \\\  -  /// |   |
-  //               | \_|  ''\---/''  |_/ |
-  //               \  .-\__  '-'  ___/-. /
-  //             ___'. .'  /--.--\  `. .'___
-  //          ."" '<  `.___\_<|>_/___.' >' "".
-  //         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-  //         \  \ `_.   \_ __\ /__ _/   .-` /  /
-  //     =====`-.____`.___ \_____/___.-`___.-'=====
-  //                       `=---='
-  //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
